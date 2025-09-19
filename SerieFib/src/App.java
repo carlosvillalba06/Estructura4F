72f730ec9c7baf3b5e9134a0c75655237659e067
RicardoOcampo06 <20243ds169@utez.edu.mx>
2025-09-18 20:51:13 -0600

    Este es el trabajo de Ocampo Vargas Ricardo Naul

---
+++ SerieFib/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static int fibonacci(int n){
+        if(n <= 1) return n;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void main(String[] args) throws Exception {
+
+        int serie = 5;
+        System.out.println("Los numeros son los siguientes: ");
+        for(int i = 0; i < serie; i++){
+            System.out.println(fibonacci(i));
+            
+        }
+    }
+}
